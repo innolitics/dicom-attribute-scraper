4f9512f8056ee29ead60d3a3e334298a6564d05e
Arjun Venkata <arjun@LAPTOP-UIJEIH8T.localdomain>
2021-01-28 18:13:01 -0600

    Clean up code and variable names

--- attribute.py
+++ attribute.py
@@ -3,60 +3,42 @@ import json
 import sys
 from pydicom.data import get_testdata_file
 
-# # fileName = "CT_small.dcm"
-excludeArray = ['OB', 'UN', 'SQ', 'OD', 'OF']
+excludeVRs = ['OB', 'UN', 'SQ', 'OD', 'OF']
 
-
-# dict_value = {'thing': [1, 2, 3, 5, 5], "alphabets": True, "last": None }
-#
-# new_obj = json.dumps(dict_value, indent=2, sort_keys=True)
-#
-# print(dict_value)
-# print(new_obj)
-#
-
-def pairTagValue(fileName, excludeTags, stringLength):
+def mapTagsToValues(fileName, excludeTags, stringLength):
     filePath = get_testdata_file(fileName)
 
-    # print(filePath)
     dataset = pydicom.dcmread(filePath)
-    print(dataset.__len__(), "\n")
+    # print(dataset.__len__(), "\n")
+
     pairing = {}
     nonPairing = {}
 
-    for m in dataset:
-        pairBool = True
+    for element in dataset:
+        isIncluded = True
         for tag in excludeTags:
-            if str(m.tag) == tag:
-                pairBool = False
+            if str(element.tag) == tag:
+                isIncluded = False
                 break
-        for VR in excludeArray:
-            if str(m.VR) == VR:
-                pairBool = False
+        for VR in excludeVRs:
+            if str(element.VR) == VR:
+                isIncluded = False
                 break
 
-        if not pairBool:
-            nonPairing[str(m.tag)] = str(m.value)[0:stringLength]
+        if not isIncluded:
+            nonPairing[str(element.tag)] = str(element.value)[0:stringLength]
         else:
-            pairing[str(m.tag)] = str(m.value)[0:stringLength]
+            pairing[str(element.tag)] = str(element.value)[0:stringLength]
 
     sets = [pairing, nonPairing]
 
-    print(pairing.__len__(), "\n")
-    print(nonPairing.__len__(), "\n")
+    # print(pairing.__len__(), "\n")
+    # print(nonPairing.__len__(), "\n")
 
     return sets
 
-
-def printPairs(fileName):
-    pairs = pairTagValue(fileName)
-
-    print(pairs[0].__len__(), pairs[0])
-    print(pairs[1].__len__(), pairs[1])
-
-
 def jsonify(fileName, excludeTags, stringLength):
-    set = pairTagValue(fileName, excludeTags, stringLength)
+    set = mapTagsToValues(fileName, excludeTags, stringLength)
     new_set = json.dumps(set, indent=2)
     print(new_set)
 
@@ -80,6 +62,5 @@ if __name__ == '__main__':
     else:
         excludeTags = [str(sys.argv[3])]
 
-
-    print(fileName, "\n")
+    # print(get_testdata_file(fileName), "\n")
     jsonify(fileName, excludeTags, stringLength)
