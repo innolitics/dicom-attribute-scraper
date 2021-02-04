9630f98d18765cf9c70ce3ec06bb310e329f9503
Arjun Venkata <arjun@LAPTOP-UIJEIH8T.localdomain>
2021-02-04 14:50:30 -0600

    Implement changes according to PR review

--- attribute.py
+++ attribute.py
@@ -3,29 +3,25 @@ import json
 import sys
 from pydicom.data import get_testdata_file
 
-exclude_VRs = ["OB", "UN", "SQ", "OD", "OF"]
+EXCLUDED_VRS = {"OB", "UN", "SQ", "OD", "OF"}
 
 
-def map_tags_to_values(file_name, exclude_tags, string_length):
+def map_tags_to_values(file_name, excluded_tags, string_length):
     file_path = get_testdata_file(file_name)
 
     data_set = pydicom.dcmread(file_path)
 
     pairing = {}
-    non_pairing = {}
 
     for element in data_set:
-        is_included = True
-        for tag in exclude_tags:
-            if str(element.tag) == tag:
-                is_included = False
-                break
-        for VR in exclude_VRs:
-            if str(element.VR) == VR:
-                is_included = False
-                break
+        group_tag = int(str(element.tag)[1:5], 16)
+        is_excluded = (
+            ((group_tag % 2) and (group_tag > 8))
+            or element.tag in excluded_tags
+            or element.VR in EXCLUDED_VRS
+        )
 
-        if is_included:
+        if not is_excluded:
             pairing[str(element.tag)] = str(element.value)[0:string_length]
 
     return pairing
@@ -33,24 +29,32 @@ def map_tags_to_values(file_name, exclude_tags, string_length):
 
 if __name__ == "__main__":
     file_name = sys.argv[1]
-    exclude_tags = ["(0008, 0008)", "(0008, 0005)", "(0008, 0012)"]
-    string_length = 0
 
     try:
         sys.argv[2]
-    except:
-        string_length = 50
+    except IndexError:
+        output_name = "out.json"
     else:
-        string_length = int(sys.argv[2])
+        output_name = sys.argv[2]
 
     try:
         sys.argv[3]
-    except:
-        exclude_tags = ["(0008, 0008)", "(0008, 0005)", "(0008, 0012)"]
+    except IndexError:
+        string_length = 50
     else:
-        exclude_tags = [str(sys.argv[3])]
+        string_length = int(sys.argv[3])
 
-    output = map_tags_to_values(file_name, exclude_tags, string_length)
+    try:
+        sys.argv[4]
+    except IndexError:
+        excluded_tags = {}
+    else:
+        excluded_tags = {str(sys.argv[4])}
 
-    with open("out.json", "w") as file:
+    output = map_tags_to_values(file_name, excluded_tags, string_length)
+
+    with open(output_name, "w") as file:
         json.dump(output, file, indent=2)
+
+    # output = pydicom.dcmread(get_testdata_file(file_name))
+    # print(output)
