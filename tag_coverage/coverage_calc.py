3832fb13000b11206fe2f0e5172fcc4f81c58904
Arjun Venkata <avenkata@innolitics.com>
2021-03-22 22:07:30 -0500

    Create script to calculate tag coverage

---
+++ tag_coverage/coverage_calc.py
@@ -0,0 +1,33 @@
+import argparse
+import json
+
+TAG_FILE = "./attributes.json"
+
+
+def calculate_coverage(in_file):
+
+    with open(TAG_FILE) as f:
+        ref_file = json.load(f)
+
+    ref_tags = [
+        x["tag"]
+        for x in ref_file
+        if x["tag"][3:5] != "XX"
+        and int(x["tag"][1:5], 16) % 2 == 0
+        and "Sequence" not in x["name"]
+    ]
+
+    your_tags = [x.upper() for x in in_file]
+
+    return len(your_tags) * 100 / len(ref_tags)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", help="JSON file for which to calculate tag coverage")
+    args = parser.parse_args()
+
+    with open(args.input_file) as f:
+        in_file = json.load(f)
+
+    print("{}%".format(calculate_coverage(in_file)))
