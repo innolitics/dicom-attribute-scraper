ac98795e2a0da5f4f7cacf483dcbf82a373023d6
Arjun Venkata <avenkata@innolitics.com>
2021-04-05 02:16:31 -0500

    Modify according to PR

--- tag_coverage/coverage_calc.py
+++ tag_coverage/coverage_calc.py
@@ -2,6 +2,7 @@
 
 Input:
 JSON file with aggregated examples for each tag
+JSON file with all possible tags, with two keys each: 'tag' and 'name'
 
 Output:
 Percentage covered out of total number of tags possible
@@ -9,33 +10,33 @@ Percentage covered out of total number of tags possible
 import argparse
 import json
 
-TAG_FILE = "./attributes.json"
 
+def calculate_coverage(input_tag_map, reference_file):
 
-def calculate_coverage(in_file):
-
-    with open(TAG_FILE) as f:
-        ref_file = json.load(f)
-
-    ref_tags = [
-        x["tag"]
-        for x in ref_file
-        if x["tag"][3:5] != "XX"
-        and int(x["tag"][1:5], 16) % 2 == 0
-        and "Sequence" not in x["name"]
+    filtered_reference_tags = [
+        data_element["tag"]
+        for data_element in reference_file
+        if data_element["tag"][3:5] != "XX"  # excludes non-specific tags
+        and int(data_element["tag"][1:5], 16) % 2 == 0  # excludes private tags
+        and "Sequence" not in data_element["name"]
     ]
 
-    your_tags = [x.upper() for x in in_file]
-
-    return len(your_tags) * 100 / len(ref_tags)
+    return len(input_tag_map) * 100 / len(filtered_reference_tags)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("input_file", help="JSON file for which to calculate tag coverage")
+    parser.add_argument(
+        "reference_file",
+        help="JSON file containing all possible tags with 'tag' and 'name' keys and string values",
+    )
     args = parser.parse_args()
 
     with open(args.input_file) as f:
-        in_file = json.load(f)
+        input_tag_map = json.load(f)
 
-    print("{}%".format(calculate_coverage(in_file)))
+    with open(args.reference_file) as f:
+        reference_file = json.load(f)
+
+    print("{:.2f}%".format(calculate_coverage(input_tag_map, reference_file)))
